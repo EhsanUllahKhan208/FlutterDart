80d5f1351218d3ba1c5557f376cc896fdc19abba
EhsanUllahKhan208 <ehsanullahkhan208@gmail.com>
2024-05-16 03:23:58 +0500

    Variables

--- dart_app/lib/main.dart
+++ dart_app/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:dart_app/Gradient_Container.dart';
 
 void main() {
   runApp(
-     MaterialApp(
+   const  MaterialApp(
       home: Scaffold(
         
         body: GradientContainer(),
@@ -11,28 +12,4 @@ void main() {
   );
 }
 
-class GradientContainer extends StatelessWidget{
-  const GradientContainer({super.key});
-  @override
-  Widget build(Context) {
-    return Container(
-          decoration:const BoxDecoration(
-            gradient: LinearGradient(
-              colors:[
-          Color.fromARGB(255, 42, 9, 104),
-          Color.fromARGB(255, 75, 49, 122)
-        ],
-        begin: Alignment.topLeft,
-        end: Alignment.bottomRight,
-         )
-        ,),
 
-        
-        
-        child: const Center(
-          child: Text("Hello Khan",style: TextStyle(color: Colors.white,fontSize: 25,),),
-        ),
-        );
-  }
-
-}
